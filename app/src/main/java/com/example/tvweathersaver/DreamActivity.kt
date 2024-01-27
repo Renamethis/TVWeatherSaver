726ffeb6ebe9214bc54306bb50fc3ffa0fd849e8
Ivan Gavrilov <ops282@mail.ru>
2024-01-27 14:17:48 +0300

    Update WeatherModule with clouds & weather processing

--- app/src/main/java/com/example/tvweathersaver/DreamActivity.kt
+++ app/src/main/java/com/example/tvweathersaver/DreamActivity.kt
@@ -1,8 +1,6 @@
 package com.example.tvweathersaver
 
-import android.Manifest
 import android.annotation.SuppressLint
-import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Handler
 import android.service.dreams.DreamService
@@ -12,25 +10,11 @@ import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.compose.ui.graphics.Color
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.app.ActivityCompat
 import androidx.tv.material3.ExperimentalTvMaterial3Api
-import com.example.library.CloudView
-import com.github.matteobattilana.weather.PrecipType
-import com.github.matteobattilana.weather.WeatherView
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationAvailability
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.Priority
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.withContext
 import org.json.JSONArray
 import org.json.JSONObject
 import java.util.Date
@@ -46,10 +30,10 @@ val scope = CoroutineScope(Job() + Dispatchers.Main);
 
 class DreamActivity : DreamService() {
     private lateinit var fusedLocation: FusedLocation;
-    private lateinit var cloudView: CloudView;
+    private lateinit var weatherModule: WeatherModule;
     private lateinit var enviroContainer: EnviroContainer;
-    private lateinit var weatherRunnable: Runnable
-    private lateinit var enviroRunnable: Runnable
+    private lateinit var weatherRunnable: Runnable;
+    private lateinit var enviroRunnable: Runnable;
     private val handler = Handler()
 
     @SuppressLint("AppBundleLocaleChanges")
@@ -62,17 +46,14 @@ class DreamActivity : DreamService() {
         isInteractive = true
         setContentView(R.layout.weather_view)
         fusedLocation = FusedLocation(applicationContext)
-        cloudView = findViewById(R.id.cloud_view)
-        enviroContainer = EnviroContainer(findViewById<ConstraintLayout>(R.id.dream_layout), applicationContext, scope, Color(
+        val layout = findViewById<ConstraintLayout>(R.id.dream_layout)
+        weatherModule = WeatherModule(scope, findViewById(R.id.cloud_view),
+            findViewById(R.id.weather_view))
+        enviroContainer = EnviroContainer(layout, applicationContext, scope, Color(
             startColor));
     }
     override fun onDreamingStarted() {
         super.onDreamingStarted()
-        //findViewById<WeatherView>(R.id.weather_view).setCustomBitmap(
-        //    BitmapFactory.decodeResource(applicationContext.getResources(), R.drawable.snowflake))
-        cloudView.setDefaults()
-        cloudView.setMinSize(300)
-        cloudView.stopAnimations()
         val timeRunnable = object : Runnable {
             @RequiresApi(Build.VERSION_CODES.Q)
             override fun run () {
@@ -88,7 +69,7 @@ class DreamActivity : DreamService() {
         weatherRunnable = object: Runnable {
             override fun run() {
                 handler.removeCallbacksAndMessages(this)
-                getWeatherByLocation()
+                updateWeatherAndClouds()
             }
         }
         enviroRunnable = object : Runnable {
@@ -100,57 +81,17 @@ class DreamActivity : DreamService() {
         handler.postDelayed(timeRunnable,100)
         handler.postDelayed(weatherRunnable, 1000)
     }
-
-    private val mutex = Mutex()
-    private fun updateWeather(url: String) {
-        if(!mutex.tryLock())
-            return
-        scope.launch {
-            val response = withContext(Dispatchers.IO) { HttpClient.get(url) }
-            val currentWeather = response?.getJSONObject("current")
-            val clouds = currentWeather?.getInt("clouds")
-            val wind = currentWeather?.getDouble("wind_speed")
-            val weather = currentWeather?.getJSONArray("weather")
-            if (clouds != null) {
-                if (clouds > 20 && !cloudView.isAnimating) { // TODO: Check if difference is enough to update animation
-                    cloudView.setPassTimeVariance(
-                        (20000.0 * (1.0 - (wind?.div(20.0) ?: 1.0))).toInt()
-                    );
-                    cloudView.setBasePassTime(
-                        (10000.0 * (1.0 - (wind?.div(20.0) ?: 1.0))).toInt()
-                    );
-                    cloudView.setCloudCount((10.0 * ((clouds.div(100.0) ?: 0.0))).toInt())
-                    if (!cloudView.isAnimating)
-                        cloudView.startAnimation();
-                } else if(clouds <= 20 && cloudView.isAnimating) {
-                    cloudView.stopAnimations();
-                }
-            }
-            weather?.let {
-                for (it in weather!!) {
-                    val weatherMain = it.getString("main")
-                    val weatherDescription = it.getString("description")
-                    if (weatherMain == "Snow")
-                        findViewById<WeatherView>(R.id.weather_view).setWeatherData(PrecipType.SNOW)
-                    else if (weatherMain == "Rain")
-                        findViewById<WeatherView>(R.id.weather_view).setWeatherData(PrecipType.RAIN)
-//                            findViewById<WeatherView>(R.id.weather_view).emissionRate = 150.0f
-                }
-            }
-            mutex.unlock();
-        }
-    }
-    @SuppressLint("MissingPermission")
-    private fun getWeatherByLocation(){
+    
+    private fun updateWeatherAndClouds(){
         val location = fusedLocation.getLocation();
         if(location != null)
-            updateWeather(
+            weatherModule.update(
                 applicationContext.getString(R.string.weather_backend_url) + "?lat=" +
                         location.latitude + "&lon=" + location.longitude + "&appid=" +
                         applicationContext.getString(R.string.apikey)
             )
         else
-            getWeatherByLocation()
+            updateWeatherAndClouds()
     }
     private fun updateTime() {
         val txtCurrentTime = findViewById<View>(R.id.time) as TextView
--- app/src/main/java/com/example/tvweathersaver/WeatherModule.kt
+++ app/src/main/java/com/example/tvweathersaver/WeatherModule.kt
@@ -1,4 +1,69 @@
 package com.example.tvweathersaver
 
-class WeatherModule {
+import com.example.library.CloudView
+import com.github.matteobattilana.weather.PrecipType
+import com.github.matteobattilana.weather.WeatherView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+
+@Suppress("NAME_SHADOWING")
+class WeatherModule(
+    private val scope: CoroutineScope,
+    private val cloudView: CloudView,
+    private val weatherView: WeatherView
+) {
+    private val mutex = Mutex()
+    init {
+        cloudView.setDefaults()
+        cloudView.setMinSize(300)
+        cloudView.stopAnimations()
+    }
+    private fun updateClouds(currentWeather: JSONObject?) {
+        val clouds = currentWeather?.getInt("clouds")
+        val wind = currentWeather?.getDouble("wind_speed")
+        if (clouds != null) {
+            if (clouds > 20 && !cloudView.isAnimating) { // TODO: Check if difference is enough to update animation
+                cloudView.setPassTimeVariance(
+                    (20000.0 * (1.0 - (wind?.div(20.0) ?: 1.0))).toInt()
+                );
+                cloudView.setBasePassTime(
+                    (10000.0 * (1.0 - (wind?.div(20.0) ?: 1.0))).toInt()
+                );
+                cloudView.setCloudCount((10.0 * ((clouds.div(100.0) ?: 0.0))).toInt())
+                if (!cloudView.isAnimating)
+                    cloudView.startAnimation();
+            } else if(clouds <= 20 && cloudView.isAnimating) {
+                cloudView.stopAnimations();
+            }
+        }
+    }
+    private fun updateWeather(currentWeather: JSONObject?) {
+        val weather = currentWeather?.getJSONArray("weather")
+        weather?.let {
+            for (it in weather) {
+                val weatherMain = it.getString("main")
+                val weatherDescription = it.getString("description")
+                if (weatherMain == "Snow")
+                    weatherView.setWeatherData(PrecipType.SNOW)
+                else if (weatherMain == "Rain")
+                    weatherView.setWeatherData(PrecipType.RAIN)
+            }
+        }
+    }
+    fun update(url: String) {
+        if(!mutex.tryLock())
+            return
+        scope.launch {
+            val response = withContext(Dispatchers.IO) { HttpClient.get(url) }
+            val currentWeather = response?.getJSONObject("current")
+            updateClouds(currentWeather);
+            updateWeather(currentWeather)
+            mutex.unlock();
+        }
+    }
+
 }
