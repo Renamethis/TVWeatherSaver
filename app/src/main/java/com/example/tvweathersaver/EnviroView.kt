937a989297784ffae439ee6b6f60fcf6740c20fd
Ivan Gavrilov <ops282@mail.ru>
2024-02-05 00:41:42 +0300

    Add coloring enviro view background depends on reaching limits

--- app/src/main/java/com/example/tvweathersaver/EnviroView.kt
+++ app/src/main/java/com/example/tvweathersaver/EnviroView.kt
@@ -6,10 +6,10 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Paint
-import android.graphics.Rect
 import android.graphics.RectF
 import android.os.Build
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import androidx.annotation.RequiresApi
 import androidx.compose.ui.graphics.Color
@@ -25,16 +25,17 @@ fun String.format(digits: Int) = "%s".format(this.substring(0, digits))
 private const val imgSize: Int = 48;
 
 class EnviroView : View {
-    private var icon: Bitmap? = null;
-    private var name: String? = null;
-    private var limits: Pair<Int, Int>? = null;
-    private var value: Float? = null;
-    private var unit: String? = null;
+    private lateinit var icon: Bitmap;
+    private lateinit var name: String;
+    private lateinit var limits: Pair<Int, Int>;
+    private var value: Float? = null
+    private lateinit var unit: String;
     private var color: Color? = null;
     private val textPaint: Paint = Paint();
 
     fun updateView(value: Float) {
         this.value = value
+
         invalidate()
     }
     @RequiresApi(Build.VERSION_CODES.O)
@@ -71,12 +72,12 @@ class EnviroView : View {
         try {
             val iconReference = parsedAttributes.getResourceIdOrThrow(R.styleable.EnviroModuleView_icon)
             icon = BitmapFactory.decodeResource(context.resources, iconReference)
-            name = parsedAttributes.getString(R.styleable.EnviroModuleView_name)
+            name = parsedAttributes.getString(R.styleable.EnviroModuleView_name).toString()
             limits = Pair<Int, Int>(
                 parsedAttributes.getIntOrThrow(R.styleable.EnviroModuleView_limitLow),
                 parsedAttributes.getIntOrThrow(R.styleable.EnviroModuleView_limitUp))
             value = parsedAttributes.getFloatOrThrow(R.styleable.EnviroModuleView_value)
-            unit = parsedAttributes.getString(R.styleable.EnviroModuleView_unit)
+            unit = parsedAttributes.getString(R.styleable.EnviroModuleView_unit).toString()
         } catch(e: IllegalArgumentException) {
             // TODO: Process exception
         }
@@ -97,6 +98,23 @@ class EnviroView : View {
         val height = paddingTop + imgSize*2 + paddingBottom
         setMeasuredDimension(width, height)
     }
+    private fun calculateBackgroundColor(): Paint {
+        val paint = Paint()
+        if(value != null) {
+            if (value!! > limits.first && value!! > limits.second)
+                paint.color = context.resources.getColor(R.color.highLimit)
+            else if (value!! < limits.second && value!! > limits.first)
+                paint.color = context.resources.getColor(R.color.lowLimit)
+            else
+                paint.color = context.resources.getColor(R.color.underLimit)
+            paint.alpha = 128
+        } else {
+            paint.color = context.resources.getColor(R.color.underLimit)
+            paint.alpha = 255
+            Log.e("TVWeatherSaver", "Error: EnviroView Value is null!")
+        }
+        return paint
+    }
     @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas) {
         icon ?: return
@@ -108,22 +126,19 @@ class EnviroView : View {
             val rectBox = RectF(
                 x, y, x + imgSize*1.5f, y + imgSize*1.5f
             );
-            val backgroundPaint = Paint()
-            backgroundPaint.color = this.color.hashCode()
-            backgroundPaint.alpha = 128
+            val backgroundPaint = calculateBackgroundColor()
             canvas.drawRoundRect(rectBox, imgSize * 2.0f, imgSize * 2.0f, backgroundPaint)
-            val bufRect = rectBox
             rectBox.left = x + 0.25f* imgSize
             rectBox.top = y + 0.25f* imgSize
             rectBox.right = x + imgSize*1.25f
             rectBox.bottom = y + imgSize*1.25f
-            canvas.drawBitmap(icon!!, null, rectBox, null)
-            val text = "${value?.format(1)} ${unit?.format(1)}"
-            val bounds = RectF(bufRect)
+            canvas.drawBitmap(icon, null, rectBox, null)
+            val text = "${value!!.format(1)} ${unit.format(1)}"
+            val bounds = RectF(rectBox)
             bounds.right = textPaint.measureText(text, 0, text.length)
             bounds.bottom = textPaint.descent() - textPaint.ascent()
-            bounds.left += (bufRect.width() - bounds.right) / 2.0f;
-            bounds.top += (bufRect.height() - bounds.bottom) / 2.0f;
+            bounds.left += (rectBox.width() - bounds.right) / 2.0f;
+            bounds.top += (rectBox.height() - bounds.bottom) / 2.0f;
 
             canvas.drawText(
                 text,
